34e1b8a9708f4701081e4cf91abb4c8040b33a67
katiafill <katiafill@mail.ru>
2024-07-17 17:16:37 +0700

    Add exception messages

--- src/main/java/ru/cft/shift/util/parameters/UtilParametersException.java
+++ src/main/java/ru/cft/shift/util/parameters/UtilParametersException.java
@@ -1,21 +1,26 @@
 package ru.cft.shift.util.parameters;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
 public class UtilParametersException extends Exception {
+    @Getter
+    @RequiredArgsConstructor
     public enum ExceptionType {
-        NO_FILES, //"Invalid command parameters. Should be filename."
-        INVALID_FILE_FORMAT, //"Invalid command parameters. File should be txt format."
-        INVALID_PREFIX_OPTION, //"Invalid command parameters. After option -p should be prefix value."
-        INVALID_PATH_OPTION, //"Invalid command parameters. After option -o should be path value."
-        INVALID_STATISTIC_OPTION //"Invalid command parameters. Option -s or -f should be once."
+        NO_FILES("Invalid command parameters. Should be filename."),
+        INVALID_FILE_FORMAT("Invalid command parameters. File should be txt format."),
+        INVALID_PREFIX_OPTION("Invalid command parameters. After option -p should be prefix value."),
+        INVALID_PATH_OPTION("Invalid command parameters. After option -o should be path value."),
+        INVALID_STATISTIC_OPTION("Invalid command parameters. Option -s or -f should be once.");
+
+        private final String message;
     }
 
     private final ExceptionType exceptionType;
 
     public UtilParametersException(ExceptionType exceptionType) {
-        super();
+        super(exceptionType.getMessage());
         this.exceptionType = exceptionType;
     }
 }
