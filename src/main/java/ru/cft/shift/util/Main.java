b5b81aba6f53b3bca95d3fd6090ecfc67b43c904
katiafill <katiafill@mail.ru>
2024-07-22 13:31:42 +0700

    Main method added.

--- src/main/java/ru/cft/shift/util/Main.java
+++ src/main/java/ru/cft/shift/util/Main.java
@@ -1,7 +1,20 @@
 package ru.cft.shift.util;
 
+import lombok.extern.slf4j.Slf4j;
+import ru.cft.shift.util.parameters.UtilParameters;
+import ru.cft.shift.util.parameters.UtilParametersException;
+import ru.cft.shift.util.service.UtilService;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            UtilParameters parameters = UtilParameters.parse(args);
+            UtilService service = new UtilService(parameters);
+            String statistic = service.run();
+            System.out.println(statistic);
+        } catch (UtilParametersException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
